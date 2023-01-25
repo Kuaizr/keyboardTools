bd1089db1d0bc7e0de36a02461527ee04a91e7c5
Kuaizr <kuaizhirui@csu.edu.cn>
2023-01-25 21:14:24 +0800

    换了UDP获取本地ipv4的方法,并且换了图标,切换了Translation的引入方式

--- Utils/UDP.py
+++ Utils/UDP.py
@@ -2,13 +2,26 @@ import socket
 from PyQt5.QtCore import *
 from Utils.Config import config
 
+def get_host_ip():
+     """
+     查询本机ip地址
+     :return: ip
+     """
+     try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+     finally:
+        s.close()
+     return ip
+
 class UDP(QThread):
     signal = pyqtSignal(list)
     def __init__(self) -> None:
         super(UDP,self).__init__()
         self.serverPort=config['UDP']['port']
         self.serverSocket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        self.ipv4 = socket.gethostbyname(socket.gethostname())
+        self.ipv4 = get_host_ip()
         self.serverSocket.bind((str(self.ipv4),self.serverPort))
         self.clientAddress = (config['UDP']['Client']['ipv4'], config['UDP']['Client']['port'])
         
@@ -19,4 +32,4 @@ class UDP(QThread):
             self.signal.emit(["failed!","客户端地址格式有问题，请修改"])
 
     def changeClient(self,ipv4,port):
-        self.clientAddress = (ipv4, int(port))
+        self.clientAddress = (ipv4, int(port))git commit -m "换了UDP获取本地ipv4的方法,并且换了图标,"
